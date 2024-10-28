8473c41bdf1e813e60f5496958dcba76c6dd8b1a
Daniel Hearn <danielh@MBP.local>
2024-10-27 22:38:07 -0500

    blog post

--- js/blog.js
+++ js/blog.js
@@ -58,16 +58,43 @@ const blogPosts = [
         "With any luck, I’ll get the IDK app up on the domain for public testing before the end of the week, and I’ll add more slides to the Conscious Effort Project page as well.\n" +
         "I’ve agonized over many of the details for over a year, so I would be happy to get feedback. -Daniel",
         imageUrl: "images/blog-image-3.jpg"
-
-    // {
-    //     title: "New Project Announcement",
-    //     date: "2024-10-27",
-    //     summary: "Exciting news about a new project in the works. ",
-    //     content: "I'm thrilled to announce that I've started working on a new project that combines AI and environmental sustainability. " +
-    //     "The project aims to develop an AI system that can analyze and provide insights on environmental data, helping us make more informed decisions about our planet's future. " +
-    //     "Stay tuned for more updates as the project progresses. - Daniel",
-    //     imageUrl: "images/blog-image-4.jpg"
-    // },
+    },
+    {
+        title: "Ideas and ADHD",
+        date: "2024-10-27",
+        summary: "Rambles about various ideas and ADHD",
+        content: 
+        "I suspect people will be more likely to want to go to in-person events such as concerts and theater performances in the near future due to the dehumanization of media. " +
+        "AI entertainment will be huge, but its overuse will push people to want to see actual people perform in front of them so they can have that connection with the content. " +
+        "I've been thinking of ways for live performers of all skill levels to be able to organize and promote their work. Maybe something like performance conventions where there " +
+        "are several stages, like giant exhibit booths separated by curtains where people can see any type of show they want? I don't know.\n" +
+        "<br>" +
+        "<br>" +
+        "I've been working out the concept of an idea I had that I call \"Phax\". Essentially, it's a way for AI assistants to communicate over phone calls for them to transmit " +
+        "data more efficiently. I'm imagining a world in which LLM-based assistants make and receive phone calls, both as personal assistants and as support agents. It would be " +
+        "better if they didn't \"talk\" using voice, but rather transmit data tonally, somewhat like fax machines or dial-up internet. The recipient agent could play three short beeps " +
+        "before the beginning of the call, which could serve two purposes. It could let humans know they're talking to an AI, and let AI agents know they can respond with handshake " +
+        "tones. They could then negotiate the parameters of the line, and data transmission could be established. What would take 5 minutes over a voice interface could be " +
+        "accomplished in less than 30 seconds, even faster if on a digital VoIP line. I guess the most difficult part is standardizing the API for integration on both sides of " +
+        "the call and convincing the platforms to implement the code universally. So far, it's just a few pages of notes and a routine daydream, but I'm fond of the idea.\n" +
+        "<br>" +
+        "<br>" +
+        "A spider weaves a web, a complex structure built from its natural urge to do so. It isn't taught, it doesn't learn. It builds the web because it knows how and needs " +
+        "to know how to in order to eat and survive. Understanding this, I think it's fair to claim that no neurological system starts at zero. A person completely deaf and blind " +
+        "will learn how to walk even if they aren't able to observe others doing it. The instantiated structure of naturally occurring neurological systems has evolved with a " +
+        "latent ability to perform actions. With future AI, we should hope to find ways to construct shapes in latent space that allow for the construction of these models " +
+        "efficiently within the network. How can we build an evolutionary system that simulates this? I've been exploring this idea as it relates to the Conscious Effort Project.\n" +
+        "<br>" +
+        "<br>" +
+        "I view having ADHD as both a blessing and a curse. The ability to take on the world and get nothing done simultaneously is a dreadful existence. I hate that I don't " +
+        "finish projects then start a new one to fool myself into thinking I'm being productive. The IDK app/project is the latest example. I've spent a few weeks banging my " +
+        "head against a wall, trying to get this simple thing to just work. All of the gears move independently but any time I try to fit them together, it locks up, motionless. " +
+        "It makes me want to give up and move on to the next thing. The next idea.\n" +
+        "<br>" +
+        "<br>" +
+        "Anyway, there are my rambles for the week. My wife and I are about to have a newborn soon, so hopefully I'll still have the brain space to keep up with these projects " +
+        "during the adjustment period. Being a father is tough but I wouldn't trade it for anything in the world. After all, children are the future. -Daniel",
+        imageUrl: "images/blog-image-4.jpg"
     },
 ];
 
