d2ac6390eb61b2e0440f8cd648827d2e9c2339c3
Daniel Hearn <danielhearn7.62@gmail.com>
2024-12-01 02:06:18 -0600

    Website overhaul

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    open: true,
-    port: 3000,
-  },
-  // base: '/daniel-hearn/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
